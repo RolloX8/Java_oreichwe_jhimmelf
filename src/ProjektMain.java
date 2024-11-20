5d42545779975e28ce900b0c3bc45209592bbacf
Jonas Himmelfreundpointner <jhimmelf@htl-steyr.ac.at>
2024-11-20 16:17:49 +0100

    Inhalt zu ProjektMain.java hinzugefügt

--- src/ProjektMain.java
+++ src/ProjektMain.java
@@ -1,4 +1,11 @@
 class ProjektMain{
     public static void main(String[] args){
+        Oreichwe oreichwe = new Oreichwe();
+
+        oreichwe.tell();
+
+        Jhimmelf jhimmelf = new Jhimmelf();
+
+        jhimmelf.tell();
     }
 }
